cc5290a430451ad98ad1dd8787a0ffda967c0acd
moxiegg <94753333+moxiegg@users.noreply.github.com>
2021-11-23 22:53:19 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <link href='https://fonts.googleapis.com/css?family=Advent Pro' rel='stylesheet'>
+        <link href='https://fonts.googleapis.com/css?family=Iceland' rel='stylesheet'>
+    </head>
+    <body>
+        <br><br><br>
+        
+        <table style="font-family:advent pro;">
+            <form action="adddetails.php" method="post">
+                <tr><td>TypeId:</td><td><input type="text" name="type"></td></tr>
+                <tr></tr><tr></tr><tr></tr><tr></tr>
+                <tr><td>Number Of Computers:<br><br></td><td><input type="number" name="num"><br><br></td></tr>
+                <tr><td><input type="submit" value="Insert"></td></tr>
+            </form>
+            <tr></tr><tr></tr><tr></tr>
+          
+        </table>
+    
+    </body>
+    </html>
