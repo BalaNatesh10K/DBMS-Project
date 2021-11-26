401267bb830a643945fb7c227dfd564a6adb4fc2
moxiegg <94753333+moxiegg@users.noreply.github.com>
2021-11-26 14:22:41 +0530

    Add files via upload

--- sno.php
+++ sno.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
-<?php
+<?php 
+    
+    $arr=[];
+    if(isset($_POST['insert'])){
+        require_once "config.php";
+        $serialNo=$_POST['serial'];
+        $sql="SELECT serial_sno,brand_name as name,processor,ram,lab_name,storage FROM comp_det2 a inner join comp_det b on a.type_id=b.type_id WHERE serial_sno='$serialNo';";
+        $result=mysqli_query($conn,$sql);
 
-$arr = [];
-if (isset($_POST['insert'])) {
-    require_once "config.php";
-    $message = $_POST['serial'];
-    $sql = "SELECT serial,a.type as type,name,ram,processor FROM test a inner join comp_det b on a.type=b.type WHERE serial='$message';";
-    $result = mysqli_query($conn, $sql);
-}
+    }    
 ?>
-
 <head>
     <title>Table with database</title>
     <link rel="stylesheet" href="disp.css">
@@ -21,30 +21,27 @@ if (isset($_POST['insert'])) {
 
         /* Style the search field */
         form.example input[type=text] {
-            float: none;
             padding: 10px;
             font-size: 17px;
-            border-radius: 5px;
+            border: 1px solid grey;
             width: 20%;
             background: #f1f1f1;
+            float:none;
         }
 
-        form.example input[type=text]:hover {
-            border: 2px solid;
-        }
         /* Style the submit button */
         form.example button {
-            float: none;
             width: 5%;
             padding: 10px;
             background: black;
             color: white;
             font-size: 17px;
-            border: 1px;
+              border: 1px ;
             border-radius: 5px;
             border-left: none;
             /* Prevent double borders */
             cursor: pointer;
+            float:none;
         }
 
         form.example button:hover {
@@ -76,26 +73,27 @@ if (isset($_POST['insert'])) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- The form -->
-        <form class="example" action="" method="POST">
-            <input type="text" placeholder="Serial number" name="serial">
-            <button type="submit" name="insert"><i class="fa fa-search"></i></button>
-        </form>
-            <?php if (isset($_POST['insert'])) {
-                echo "<table>
+        <form class="example" action="" method="post">
+            <input type="text" placeholder="Serial Number" name="serial" required>
+            <button type="submit" name='insert'><i class="fa fa-search"></i></button>
+        </form><br>
+        <?php if(isset($_POST['insert'])){
+              echo"<table style='width:80%'>
               <tr>
-                  <th>Serial</th>
-                  <th>Type_ID</th>
-                  <th>Name</th>
+                  <th>SerialNo</th>
+                  <th>Brand Name</th>
                   <th>Processor</th>
                   <th>Ram</th>
+                  <th>Storage</th>
+                  <th>Lab Name</th>
               </tr>";
-                while ($row = mysqli_fetch_assoc($result)) {
-
-                    echo "<tr><td>" . $row["serial"] . "</td><td>" . $row["type"] . "</td><td>"
-                        . $row["name"] . "</td><td>" . $row['processor'] . "</td><td>" . $row["ram"] . "</td></tr>";
-                }
-                echo "</table>";
-            } ?>
+              while($row=mysqli_fetch_assoc($result)){
+                
+                echo "<tr><td>" . $row["serial_sno"] . "</td><td>" . $row["name"] . "</td><td>"
+                  . $row["processor"] . "</td><td>" . $row['ram'] . "</td><td>" . $row["storage"] . "</td><td>" . $row['lab_name'] . "</td></tr>";
+              }
+              echo "</table>";
+            }?>
     </center>
 </body>
 
